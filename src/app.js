abd79f2b10148930aecd1d9454114a777f2289bd
Andre luiz <andre.bluiz94@gmail.com>
2020-02-09 19:57:52 -0300

    set port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path')
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.NODE_PORT || 3000
+const PORT = process.env.NODE_PORT || 80
 
 app.get('/', function (req,res){
     res.sendFile(path.resolve('./html/index.html'))
